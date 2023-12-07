abee3bea03d855a5ab9f884aa28043f82abd99bf
AidanMW22 <aidan.williams@hva.nl>
2023-12-07 14:29:43 +0100

    veranderingen

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,21 @@
 // JavaScript Document
 console.log("hi");
+
+var openSearchButton = document.querySelector("header > button");
+var search= document.querySelector("header nav");
+
+openSearchButton.onclick = openMenu;
+function openMenu() {
+    search.classList.add("open")
+}
+
+
+
+
+var openMenuButton = document.querySelector("header > button");
+var deMenu= document.querySelector("header nav");
+
+openSearchButton.onclick = openMenu;
+function openMenu() {
+    search.classList.add("open")
+}
